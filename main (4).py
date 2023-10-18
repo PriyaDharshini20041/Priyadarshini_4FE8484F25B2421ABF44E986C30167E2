bcb6fe3fdfacf67be8d1fc3ef54c37d0feccfe1e
PriyaDharshini20041 <148313234+PriyaDharshini20041@users.noreply.github.com>
2023-10-18 12:19:19 +0530

    Update main (4).py

--- main (4).py
+++ main (4).py
@@ -1,19 +1,10 @@
-class Student:
-    def __init__(self, name, roll_number, cgpa):
-        self.name = name
-        self.roll_number = roll_number
-        self.cgpa = cgpa
+def fact_rec(n):
+  if n == 0 or n == 1:
+    return 1
+  else:
+    return n * fact_rec(n - 1)
 
-def sort_students(student_list):
-    sorted_students = sorted(student_list, key=lambda x: x.cgpa, reverse=True)
-    return sorted_students
-if __name__ == "__main__":
-    students = [
-        Student("Alice", "101", 3.9),
-        Student("Bob", "102", 3.7),
-        Student("Charlie", "103", 3.5),
-        Student("David", "104", 3.8),
-    ]
-    sorted_students = sort_students(students)
-    for student in sorted_students:
-        print(f"Name: {student.name}, Roll Number: {student.roll_number}, CGPA: {student.cgpa}")
+
+number = 12
+res = fact_rec(number)
+print("The factorialof {} is {}.".format(number, res))
